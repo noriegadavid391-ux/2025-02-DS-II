228e050f487351ce114c15d2d3a838d1643ea9fd
joevalen2033 <joevalen@bancolombia.com.co>
2025-09-06 11:51:47 -0500

    Codigo inicial

--- Spring/spring-mvc/src/main/java/com/example/spring_mvc/SpringMvcApplication.java
+++ Spring/spring-mvc/src/main/java/com/example/spring_mvc/SpringMvcApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.spring_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringMvcApplication.class, args);
 	}
 
 }
--- Spring/spring-mvc/src/test/java/com/example/spring_mvc/SpringMvcApplicationTests.java
+++ Spring/spring-mvc/src/test/java/com/example/spring_mvc/SpringMvcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.spring_mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringMvcApplicationTests {
 
 	@Test
 	void contextLoads() {
